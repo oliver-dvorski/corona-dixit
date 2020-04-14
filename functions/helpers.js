6df062f8a7e7a527fb4db6cba1567fb28ea9922a
oliver-dvorski <oliver.dvorski@gmail.com>
2020-04-14 18:42:57 +0200

    Extract the helper functions

---
+++ functions/helpers.js
@@ -0,0 +1,24 @@
+async function getHand(deck) {
+  const hand = [];
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * deck.length);
+
+    hand.push(deck[randomIndex]);
+
+    deck.splice(randomIndex, 1);
+  }
+
+  return hand;
+}
+
+async function getFullDeck() {
+  const storageCollection = await storage.bucket('corona-dixit.appspot.com').getFiles();
+
+  return storageCollection[0].map((storageObject) => storageObject.name);
+}
+
+module.exports = {
+  getHand,
+  getFullDeck,
+};
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const { Storage } = require('@google-cloud/storage');
+const { getHand, getFullDeck } = require('./helpers');
 
 const serviceAccount = require('./service-account.json');
 
@@ -12,26 +13,6 @@ admin.initializeApp(adminConfig);
 const storage = new Storage({
   keyFilename: './service-account.json',
 });
-
-async function getHand(deck) {
-  const hand = [];
-
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor(Math.random() * deck.length);
-
-    hand.push(deck[randomIndex]);
-
-    deck.splice(randomIndex, 1);
-  }
-
-  return hand;
-}
-
-async function getFullDeck() {
-  const storageCollection = await storage.bucket('corona-dixit.appspot.com').getFiles();
-
-  return storageCollection[0].map((storageObject) => storageObject.name);
-}
 
 exports.shuffleCards = functions
   .firestore
