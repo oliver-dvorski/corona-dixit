a9921fb2d7a4214d37998a3e4c6dcf5c3d1335cc
oliver-dvorski <oliver.dvorski@gmail.com>
2020-04-23 23:17:18 +0200

    Calculate results in a cloud function

--- src/views/Round/Results.vue
+++ src/views/Round/Results.vue
@@ -60,8 +60,7 @@
 </template>
 
 <script>
-import firebase from 'firebase/app';
-import { auth, db } from '../../firebase';
+import { db } from '../../firebase';
 import { getEmptyRound } from '../../utils/data';
 import Loader from '../../components/Loader.vue';
 
@@ -123,10 +122,6 @@ export default {
         poolSnap.forEach((item) => {
           this.pool.push(item.data());
         });
-
-        if (this.pool.length === 6 && this.round.results.length === 0 && auth.currentUser.uid === this.round.storyTeller.uid) {
-          await this.calculateResults();
-        }
       }
     },
 
@@ -142,83 +137,6 @@ export default {
           },
         };
       }
-    },
-  },
-
-  methods: {
-    async calculateResults() {
-      if (this.pool.length === 0) {
-        return false;
-      }
-
-      this.loading = true;
-
-      const storyPoolItem = this.pool.find((item) => item.setBy === this.round.storyTeller.uid);
-
-      if (storyPoolItem.chosenBy.length === 0 || storyPoolItem.chosenBy.length === this.members.length - 1) {
-        // Everyone or no one voted for the right card
-        for (const member of this.members) {
-          if (member.id !== this.round.storyTeller.uid) {
-            await db
-              .collection('rooms')
-              .doc(this.$route.params.roomID)
-              .collection('members')
-              .doc(member.id)
-              .update({
-                score: firebase.firestore.FieldValue.increment(2),
-              });
-          }
-        }
-      } else {
-        for (const playerID of storyPoolItem.chosenBy) {
-          await db
-            .collection('rooms')
-            .doc(this.$route.params.roomID)
-            .collection('members')
-            .doc(playerID)
-            .update({
-              score: firebase.firestore.FieldValue.increment(3),
-            });
-        }
-
-        await db
-          .collection('rooms')
-          .doc(this.$route.params.roomID)
-          .collection('members')
-          .doc(this.round.storyTeller.uid)
-          .update({
-            score: firebase.firestore.FieldValue.increment(3),
-          });
-      }
-
-      // Bonus points
-      for (const poolItem of this.pool) {
-        if (poolItem.setBy !== 'cloudFunction' && poolItem.setBy !== this.round.storyTeller.uid && poolItem.chosenBy.length > 0) {
-          await db
-            .collection('rooms')
-            .doc(this.$route.params.roomID)
-            .collection('members')
-            .doc(poolItem.setBy)
-            .update({
-              score: firebase.firestore.FieldValue.increment(poolItem.chosenBy.length),
-            });
-        }
-      }
-
-      await this.writeResultsToRound();
-
-      this.loading = false;
-    },
-
-    async writeResultsToRound() {
-      await db
-        .collection('rooms')
-        .doc(this.$route.params.roomID)
-        .collection('rounds')
-        .doc(this.$route.params.roundID)
-        .update({
-          results: this.members,
-        });
     },
   },
 };
